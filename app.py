2a09831a08e35c0bce6e463bb8b33222102ee240
Yishen Chen <cyt046@gmail.com>
2015-01-20 19:16:41 -0600

    commented

--- app.py
+++ app.py
@@ -8,18 +8,26 @@ import cgi
 import copy
 from config import AUTH_BASE, API_BASE, CLIENT_SECRET, CLIENT_ID, REDIRECT_URI
 
+# we use this to shorten a long resource reference when displaying it
 MAX_LINK_LEN = 20
 # we only care about genomic stuff here
 REF_RE = re.compile(r'^(?:Condition|Patient|Sequence|Procedure|Observation)/.*$')
+# list of scopes we need
+SCOPES = ['user/Sequence.read',
+        'user/Observation.read',
+        'user/Condition.read',
+        'user/Patient.read',
+        'user/Procedure.read']
 
 app = Flask(__name__)
-scopes = ['user/Sequence.read', 'user/Observation.read',
-        'user/Condition.read', 'user/Patient.read', 'user/Procedure.read']
 
 class OAuthError(Exception):
     pass
 
 def get_access_token(auth_code):
+    '''
+    exchange `code` with `access token`
+    '''
     exchange_data = {
         'code': auth_code,
         'redirect_uri': REDIRECT_URI,
@@ -34,11 +42,18 @@ def get_access_token(auth_code):
         return resp.json()['access_token']
 
 def api_call(api_endpoint):
+    '''
+    helper function that makes API call 
+    '''
     access_token = request.cookies['access_token']
     auth_header = {'Authorization': 'Bearer %s'% access_token}
     return requests.get('%s%s'% (API_BASE, api_endpoint), headers=auth_header)
 
 def to_internal_id(full_id):
+    '''
+    markup an internal resource id with anchor tag.
+    e.g. change Patient/123 into <a href='...'>Patient/123</a>
+    '''
     if not full_id.startswith(API_BASE):
         internal_id = full_id
     else:
@@ -52,13 +67,15 @@ def has_access():
     '''
     if 'access_token' not in request.cookies:
         return False
+    # we are being lazy here and don't keep a status of our access token,
+    # so we just make a simple API call to see if it expires yet
     test_call = api_call('/Patient?_count=1') 
     return test_call.status_code != 403
 
 
 def render_fhir(resource):
     '''
-    render a "nice" view of FHIR bundle
+    render a "nice" view of a FHIR bundle
     '''
     for entry in resource['entry']:
         entry['id'] = to_internal_id(entry.get('id', ''))
@@ -67,6 +84,12 @@ def render_fhir(resource):
 
 
 def make_links(resource):
+    '''
+    scans a resource and replace internal resource references with anchor tags pointing to them
+    e.g. turn {'reference': 'Patient/123'} into {'reference': '<a href="...">"Patient/123"</a>'}
+
+    we are not reusing `def to_internal_id` here because that function shortens an id for styles
+    '''
     for k, v in resource.iteritems():
         if isinstance(v, dict):
             make_links(v)
@@ -91,20 +114,18 @@ def get_code_snippet(resource):
 
 
 def require_oauth(view):
-
     @wraps(view)
     def authorized_view(*args, **kwargs):
+        # check is we have access to the api, if not, we redirect to the API's auth page
         if has_access():
             return view(*args, **kwargs)
         else:
             redirect_args = {
-                'scope': ' '.join(scopes),
+                'scope': ' '.join(SCOPES),
                 'client_id': CLIENT_ID,
                 'redirect_uri': REDIRECT_URI,
                 'response_type': 'code'}
-            return redirect('%s/%s?%s'% (AUTH_BASE,
-                                        'authorize',
-                                        urlencode(redirect_args)))
+            return redirect('%s/authorize?%s'% (AUTH_BASE, urlencode(redirect_args)))
     
     return authorized_view
 
@@ -146,6 +167,7 @@ def forward_api(forwarded_url):
         }
     elif len(bundle.get('entry', [])) > 0:
         bundle['resourceType'] = bundle['entry'][0]['content']['resourceType']
+
     return render_fhir(bundle)
 
 
