00673546e6a95bd1d7b19d2f1c970ab13f12e25b
Yishen Chen <cyt046@gmail.com>
2015-05-26 21:24:40 -0400

    pointed default config to our demo server

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # replace this with your stuff
-API_BASE = 'http://localhost:5000/api'
-AUTH_BASE = 'http://localhost:5000/auth'
-CLIENT_ID  = 'eeb54273-c606-4c26-a6a7-a34486447182'
+API_BASE = 'http://genomics-advisor.smartplatforms.org:5000/api'
+AUTH_BASE = 'http://genomics-advisor.smartplatforms.org:5000/auth'
+CLIENT_ID  = 'd400999c-9c39-4523-bbf4-660e7d8a89dc'
 REDIRECT_URI = 'http://localhost:8000/recv_redirect'
