5cbfa8aefa4477194fe5cec3535f61b793d3ca0f
chern <cxhjonhson@gmail.com>
2016-02-09 00:26:35 +0800

    02/09/2016

--- load_example.py
+++ load_example.py
@@ -1,26 +1,26 @@
 example_policy = {
-	"Sequence":{
-		"resourceType": "Sequence",
-   	 	"observedAllele": "masked",
-		"variationID": {
-        		"coding": [
-            			{
-                			"code": "masked"
-				}
-			]
-		}
-	},
-	"Patient":{
-		"name": [
-        	{
-            	"text": "Garret Doremus"
-        	}
-    	],
-    	"gender": "male"
-	}
+    "Patient":{
+    "name": [
+        {
+            "text": "Victor Bland"
+        }
+    ],
+    "resourceType": "Patient",
+    "text": {
+        "status": "generated",
+        "div": "<div><p>Victor Bland</p></div>"
+    },
+    "meta": {
+        "versionID": 1,
+        "lastUpdated": "2016-01-13T15:31:28.723332"
+    },
+    "gender": "male",
+    "id": "4b9f9ab9-d723-45d2-b185-3439a7f51690"
+    }
 }
 
-example_id= "4d5532fa-d5a1-42bf-82ea-3a223f2cce0b"
+
+example_id= "4b9f9ab9-d723-45d2-b185-3439a7f51690"
 
 
 
