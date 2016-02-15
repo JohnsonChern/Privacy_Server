6fefb4afa8b86344e2ab46bea8cd14572eaf2186
chern <cxhjonhson@gmail.com>
2016-02-15 23:37:46 +0800

    02/15/2016

--- config.py
+++ config.py
@@ -2,11 +2,14 @@
 
 API_BASE = 'http://genomics-advisor.smartplatforms.org:2048/api'
 AUTH_BASE = 'http://genomics-advisor.smartplatforms.org:2048/auth'
+"""
 <<<<<<< HEAD
 =======
 
 #API_BASE = 'http://localhost:5000/api'
 #AUTH_BASE = 'http://localhost:5000/auth'
 >>>>>>> c4c3fbf82546576cab2d02943f4397b436904a6a
+"""
+
 CLIENT_ID  = '5974e104-619a-4970-b8d8-7b0a813983fa'
 REDIRECT_URI = 'http://localhost:8000/recv_redirect'
