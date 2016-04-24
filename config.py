91c083913f11e5e320996020978d345d010094e8
Reimilia <panzer.wy@gmail.com>
2016-04-24 00:25:17 -0700

    just commit for merge

--- config.py
+++ config.py
@@ -6,10 +6,19 @@
 API_BASE = 'http://genomics-advisor.smartplatforms.org:2048/api'
 AUTH_BASE = 'http://genomics-advisor.smartplatforms.org:2048/auth'
 
+#API_BASE = 'http://localhost:2048/api'
+#AUTH_BASE = 'http://localhost:2048/auth'
+
 PRIVACY_BASE = 'http://localhost:5000/Privacy'
 #AUTH_BASE = 'http://localhost:5000/auth'
 
+#CLIENT_ID = '30d98168-9862-487b-9f87-4111c44f61cf'
+
+# 7191*****
 CLIENT_ID  = '5974e104-619a-4970-b8d8-7b0a813983fa'
+
+# 7999*****
+#CLIENT_ID = '90169159-a66b-4439-9f73-484b288ec2a5'
 
 REDIRECT_URI = 'http://localhost:8000/recv_redirect'
 
